5dab1c3d830787c4fbf42f53f9118a36d169d487
HuckleberryTerry <104868577+HuckleberryTerry@users.noreply.github.com>
2025-01-14 16:08:16 +0000

    EOD
    Fixed:
    Delete Images Functionality - Removed button. it is not working as intended.
    
    Delete Collections Functionality - Fixed the delete collections button so it removes all images from the collection related to the selected profile
    
    View Collection Functionality - Made the view collection display only the currently selected profiles images.

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
     var rand = Math.floor(Math.random() * 100);
     var deviceWidth = window.innerWidth - 50;
     console.log(rand);
-    var img = `<img class="new-image" src="https://picsum.photos/id/${rand}/${deviceWidth}/650">`;
+    var img = `<img class="new-image" src="https://picsum.photos/id/${rand}/${deviceWidth}/700">`;
     //$image_Wrapper.append(img);
     $image_Wrapper.prepend(img);
 })
@@ -22,7 +22,7 @@ $form_Submit.on('click', function(event){
         var text = $form_Email.val();
         emailArray.push(text);
         console.log(emailArray);
-        var option = `<option> ${text} </option>`;
+        var option = `<option selected> ${text} </option>`;
         $form_Email.val('');
         $email_List.append(option);
         $form_Email.attr('oninvalid', 'this.setCustomValidity("Email Has Been Uploaded")');
@@ -45,7 +45,7 @@ $form_Submit.on('click', function(event){
         //alert('Incorrect Email has been entered. Please enter a valid email address');
         $form_Email.attr('oninvalid', 'this.setCustomValidity("Email is Invalid")');
         $form_Email.attr('oninput', ' ');
-        //event.preventDefault();
+        event.preventDefault();
     }
 })
 
@@ -60,9 +60,9 @@ function isEmailExist(email){
 
 function generateNewImage(){
     const $newImage = $('.new-image');
-    var rand = Math.floor(Math.random() * (1081 - 100) + 100);
+    var rand = Math.floor(Math.random() * (1081 - 1) + 1);
     var deviceWidth = window.innerWidth - 50;
-    var randomImage = `https://picsum.photos/id/${rand}/${deviceWidth}/650`
+    var randomImage = `https://picsum.photos/id/${rand}/${deviceWidth}/700`
     $newImage.attr('src', randomImage);
 }
 
@@ -86,21 +86,143 @@ function saveImage(){
 }
 
 let userArray = [];
+let collectionNo = 0;
+let collectionUserNo = 0;
 function saveToCollection(){
     const $newImage = $('.new-image');
     let tempImg = $newImage.attr('src');
-
-    console.log($email_List.val());
-    userArray.push([$email_List.val(), tempArray]);
-    $collections_Stored.append(`<div class='${userArray[0]}-collection collections-container'>
-                                    <a href='#'>
-                                        <img src='${tempArray[0]}'>
-                                        <p>${$email_List.val()}</p>
-                                    </a>
-                                </div>`);
+    //console.log($email_List.val());
+    userArray.push([{emailAddress:$email_List.val(), imageSources:tempArray}]);
     console.log(userArray);
+    // $collections_Stored.append(`<div class='${collectionNo}-collection collections-container'>
+    //                                 <a href='#'>
+    //                                     <img src='${tempArray[0]}'>
+    //                                     <p class='collection-title'>${$email_List.val()}</p>
+    //                                 </a>
+    //                             </div>`);
+    //console.log(userArray);
+    collectionNo += 1;
     $collections_Images.children().remove();
     tempArray = [];
+    $collections_Stored.show();
+}
+
+function getUserImages(arr){
+    $collections_Stored.children().remove();
+    for(let key in arr){
+        if(typeof arr[key] === "object"){
+            for (let nestedKey in arr[key]) {
+                //console.log(arr[key][nestedKey]);
+                // console.log(arr[key][nestedKey].imageSources.length);
+                if('imageSources' in arr[key][nestedKey]){
+                    for(let i = 0; i < arr[key][nestedKey].imageSources.length; i++){
+                        //console.log(arr[key][nestedKey].imageSources[i]);
+                        if(arr[key][0].emailAddress === $email_List.val()){
+                            //console.log(arr[key][0].emailAddress);
+                            document.querySelector('#collections-title').innerHTML = `Collections - ${arr[key][0].emailAddress}`
+                            $collections_Stored.append(`<a href='#' onClick='selectedImage()'>
+                                <img id='${i}' class='collection-saved-image' src=${arr[key][nestedKey].imageSources[i]}>
+                            </a>`);
+                        }
+                    }
+                }
+            }
+        } 
+        else {
+            //console.log(array[key]);
+        }
+    }
+
+    // for(var i = 0; i < arr.length; i++){
+    //     if(arr[i][0].emailAddress === $email_List.val()){
+    //         //console.log(arr[i][1]);
+    //         //console.log('checked');
+    //         for(let j = 0; j < arr[i].length; j++){
+    //             console.log('checked');
+    //             $collections_Stored.append(`<a href='#' onClick='selectedImage()'>
+    //                 <img class='collection-saved-image' src=${arr[0][i].imageSources[j]}>
+    //             </a>`);
+    //         }
+    //         //$collections_Stored.children().remove();
+    //         // for(var j = 0; j < arr[i][1].length; j++){
+    //         //     //console.log(arr[i][1][j]);
+    //         //     //tempArray.push(arr[i][1][j]);
+    //         //     //console.log(tempArray);
+    //         // }
+    //         //$collections_Stored.hide();
+    //         //$collections_Images.append(`<img src=${arr[i][1]}>`);
+    //         //Display Collection
+    //     }
+    // }
+}
+
+let selectedImageArr = [];
+function selectedImage(){
+    let $collectionSavedImage = $('.collection-saved-image').attr('src');
+    // console.log($collectionSavedImage.attr('src'));
+    // selectedImageArr.push($collectionSavedImage.attr('src'));
+    // console.log(selectedImageArr);
+    console.log($collectionSavedImage);
+}
+
+function deleteCollection(arr){
+    document.querySelector('#collections-title').innerHTML = 'Collections';
+    for(let key in arr){
+        if(typeof arr[key] === "object"){
+            for (let nestedKey in arr[key]) {
+                //console.log(arr[key][0].emailAddress);
+                if(arr[key][0].emailAddress === $email_List.val()){
+                    //show modal asking if the user wants to delete the collection for user
+                    delete arr[key][0].emailAddress;
+                    delete arr[key][0].imageSources;
+                    // let ETD = emailArray.indexOf($email_List.val());
+                    // if(ETD > -1){
+                    //     emailArray.splice(ETD, 1);
+                    // }
+                    // let $email_List_Selected = $('.options :selected');
+                    // $email_List_Selected.remove();
+                    $collections_Stored.children().remove();
+                    console.log(arr[key]);
+                }
+                else{
+                    console.log('mismatch');
+                }
+                console.log(arr[key]);
+              }
+        } 
+        else {
+            //console.log(array[key]);
+        }
+    }
+}
+
+function deleteSelectedImage(arr1, arr2){
+    //console.log(arr1);
+    for(let key in arr2){
+        if(typeof arr2[key] === "object"){
+            for (let nestedKey in arr2[key]) {
+                //console.log(arr2[key][nestedKey]);
+                // console.log(arr[key][nestedKey].imageSources.length);
+                for(let i = 0; i < arr2[key][nestedKey].imageSources.length; i++){
+                    //console.log(arr2[key][nestedKey].imageSources[i]);
+
+                }
+              }
+        } 
+        else {
+            //console.log(array[key]);
+        }
+    }
+
+    // for(let i = 0; i < arr1.length; i++){
+    //     for(let j = 0; j < arr1.length; j++){
+    //         if(arr2.imageSources.includes(arr1[i])){
+    //             console.log('contains');
+    //             $collectionSavedImage.addClass('selected');
+    //             $collectionSavedImage.remove('.selected');
+    //         }
+    //     }
+    // }
 }
 
 //To Do List
@@ -108,10 +230,10 @@ function saveToCollection(){
 //   Display an error message below the save button. !---Completed---!
 
 // - If everything is as it should be, then the save button saves the image to a temporary collection.
-//   which is linked to their email address. This should be an array of arrays.
+//   which is linked to their email address. This should be an array of arrays. !---Completed---!
 
 // - Add in a delete button that allows a user to delete collections/images in a collection.
 //   This should be a simple get function that matches an image id with another in the array
-//   and then deletes the matching entry.
+//   and then deletes the matching entry. !---in progress---!
 
  
